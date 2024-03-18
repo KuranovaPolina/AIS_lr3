14843c4fa678d9d2b8478d048e2e9bc5899ec4f3
Polina <kuranovapolina@gmail.com>
2024-03-18 23:18:01 +0300

    added /2 route for index2.html

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/2')
+def hello2():
+    return render_template('index2.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7878)
